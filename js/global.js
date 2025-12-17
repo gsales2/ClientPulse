7ef4984c5fdb0569461d3808681a1fae52c0a156
gsales2 <gabrielsales2002@gmail.com>
2025-12-17 19:54:49 -0300

    Remove codigos redundantes

--- js/global.js
+++ js/global.js
@@ -1,4 +1,3 @@
 export const menuBar = document.querySelector('#menu-bar');
 export const nav = document.querySelector('#nav');
 export const menuBg = document.querySelector('#menu-bg')
-export const body = document.body
